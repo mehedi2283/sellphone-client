bd2903b2b8caeaa4753f8f5c4ac4b22d1169df76
Mehedi Hasan <mehedihasan123456789.mh.mh@gmail.com>
2022-11-27 20:50:13 +0600

    seller product status added

--- src/components/ProductCard/ProductCardForSeller.js
+++ src/components/ProductCard/ProductCardForSeller.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { GoUnverified, GoVerified } from "react-icons/go";
 import OrderModal from "../BookNowModal/BookNowModal";
 import { AuthContext } from "./../../context/AuthProvider/AuthProvider";
 
@@ -28,33 +29,38 @@ const ProductCardForSeller = ({
                 <img src={picture} alt={name} />
             </figure>
             <div className="card-body">
-                <h2 className="text-2xl font-bold text-center">{name}</h2>
-                <p className="text-center">{location}</p>
+            <h2 className="text-2xl font-bold  text-center">{name}</h2>
+                <p className="text-center text-base font-medium">Location: {location}</p>
                 <div className="flex ">
-                    <p className="text-center w-1/2">
+                    <p className="text-center text-base font-medium w-1/2">
                         Resale Price: {resale_price} tk
                     </p>
-                    <p className="text-center w-1/2">
+                    <p className="text-center text-base font-medium w-1/2">
                         Original Price: {original_price} tk
                     </p>
                 </div>
                 <div className="flex ">
-                    <p className="text-center w-1/2">
+                    <p className="text-center text-base font-medium w-1/2">
                         Years of use: {years_of_use} years
                     </p>
-                    <p className="text-center w-1/2">
+                    <p className="text-center text-base font-medium w-1/2">
                         Post date: {posted_time}{" "}
                     </p>
                 </div>
-                <div className="flex ">
-                    <p className="text-center w-1/2">
-                        Sellers Name: {seller_name}{" "}
-                    </p>
-                    {isVerified ? (
-                        <p className="text-center w-1/2 ">verified </p>
+                <div className="flex justify-center gap-2 items-center">
+                <h2 className="text-center text-lg font-bold uppercase">
+                       <span className="normal-case"> Sellers Name</span>: {seller_name}{" "}
+                    </h2>
+                    {!isVerified ? (
+                        <GoUnverified className="text-xl"></GoUnverified>
                     ) : (
-                        <p className="text-center w-1/2">not verified </p>
+                        <GoVerified className="text-xl text-blue-500" ></GoVerified>
                     )}
+                </div>
+                <p className="divider text-lg font-semibold">Status</p>
+                <div className="flex justify-around">
+                    <button className="btn btn-outline border-0 hover:bg-green-600 text-green-600 bg-green-200">Available</button>
+                    <button className="btn btn-outline border-0 hover:bg-red-600 text-red-600 bg-red-200">Delete</button>
                 </div>
                 
             </div>
