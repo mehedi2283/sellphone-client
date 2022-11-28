7c87785fac7727257ee581eb5829c68f4605f394
Mehedi Hasan <mehedihasan123456789.mh.mh@gmail.com>
2022-11-28 13:37:46 +0600

    added loader

--- src/components/dashboard/Payment/Payment.js
+++ src/components/dashboard/Payment/Payment.js
@@ -1,7 +1,7 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 import CheckoutForm from "./CheckoutForm";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
@@ -9,8 +9,25 @@ const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
     const paymentData = useLoaderData();
+    const navigation = useNavigation()
 
     console.log('helllooo',paymentData);
+    if(navigation.state === "loading"){
+        return(
+            <div className="border my-72  border-primary/90 shadow rounded-md p-4 max-w-sm w-full mx-auto  ">
+            <div className="animate-pulse flex space-x-4">
+                <div className="rounded-full bg-primary h-12 w-12"></div>
+                <div className="flex-1 space-y-4 py-1">
+                    <div className="h-4 bg-primary rounded w-3/4"></div>
+                    <div className="space-y-2">
+                        <div className="h-4 bg-primary rounded"></div>
+                        <div className="h-4 bg-primary rounded w-5/6"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    }
     return (
         <>
             <div>
